43f6aad8900602cd323bfa2b18d9256e7baae372
Jorge Cabrera <alu0101351773@ull.edu.es>
2024-05-22 18:14:13 +0100

    Fichero de generacion de directorios realizado, directorios plantilla utilizados finalmente

--- src/project/folder_generator.py
+++ src/project/folder_generator.py
@@ -10,22 +10,25 @@ MAX_FLOW = 0.6
 FLOW_SAMPLES = 15
 
 CONFIG_FILE_TEMPLATE = {
-    'simulation': {'registers': 100},
+    'simulation': {'registers': 10_000},
     'tanks': {
         'flow_value': None,
         'vol': 50_000
     }
 }
 
-# TODO: Hacer que el fichero pueda ser ejecutado desde cualquier ruta
 script_dir = os.path.dirname(os.path.abspath(__file__))
 
 for i, val in enumerate(np.linspace(MIN_FLOW, MAX_FLOW, FLOW_SAMPLES)):
-    dir_route = f'{FOLDER_LOCATION}/case_{i:0>4}'
+    dir_route = f'{script_dir}/{FOLDER_LOCATION}/case_{i:0>4}'
     try:
         os.makedirs(dir_route)
         print(f'Directorio \'{dir_route}\' creado correctamente')
     except Exception as e:
         print(f'Error al crear el directorio: {e}')
 
-    # TODO: Incluir que el script guarde en cada directorio su fichero de configuración
+    CONFIG_FILE_TEMPLATE['tanks']['flow_value'] = float(val)
+    toml.dump(CONFIG_FILE_TEMPLATE, open(f'{dir_route}/sim_config.toml', 'w'))
+    print('\tFichero de configuración \'sim_config.toml\' generado correctamente')
+
+    print()
